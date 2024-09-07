f6332494a8fa2a00e2a3a611f6e96ca348e2bbc8
Force <maxfunmaxfun672@gmail.com>
2024-09-07 16:56:28 +0300

    added OPTIONS method

--- Blum/Core/FakeWebClient.cs
+++ Blum/Core/FakeWebClient.cs
@@ -110,6 +110,29 @@ namespace Blum.Core
             SetHeader("User-Agent", _currentProfile.UserAgent);
         }
 
+        public async Task<(RestResponse? RestResponse, string? ResponseContent, Exception? Exception)> TryOptionsAsync(string url)
+        {
+            RestResponse? response = null;
+            try
+            {
+                var request = new RestRequest(url, Method.Options);
+
+                if (_headers != null && _headers.Any())
+                    request.AddHeaders(_headers);
+
+                response = await _client.ExecuteAsync(request);
+
+                if (response.IsSuccessful)
+                    return (response, response.Content, null);
+                else
+                    return (response, null, null);
+            }
+            catch (Exception e)
+            {
+                return (response, null, e);
+            }
+        }
+
         public async Task<(RestResponse? RestResponse, string? ResponseContent, Exception? Exception)> TryGetAsync(string url)
         {
             RestResponse? response = null;
