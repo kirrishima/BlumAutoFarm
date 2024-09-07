93506aa089301f6303db4cb7ebeedbfcd4f68d57
Force <maxfunmaxfun672@gmail.com>
2024-09-07 19:28:51 +0300

    added global file paths for session and session log files

---
+++ Blum/Models/TelegramSessionsPaths.cs
@@ -0,0 +1,12 @@
+﻿namespace Blum.Models
+{
+    internal class TelegramSessionsPaths
+    {
+        public static readonly string SessionsFolder = "sessions";
+
+        public static string GetWTelegramLogFilePath(string accountName)
+        {
+            return $"{accountName} WTelegram logs.txt";
+        }
+    }
+}
