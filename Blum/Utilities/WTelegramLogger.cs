7709ca9dcb633fc63460768851b76b437af7474d
Force <maxfunmaxfun672@gmail.com>
2024-09-08 13:27:58 +0300

    fixed output, fixed bug with sessions folder

--- Blum/Utilities/WTelegramLogger.cs
+++ Blum/Utilities/WTelegramLogger.cs
@@ -4,53 +4,49 @@ namespace Blum.Utilities
 {
     internal class WTelegramLogger
     {
-        private static readonly object _fileLock = new();
-        private static readonly object _consoleLock = new();
         private static readonly object _logLock = new();
         private readonly StreamWriter _streamWriter;
-
-        private readonly string _accountName;
 
         private static readonly ConsoleColor[] WTelegramConsoleColor = [ ConsoleColor.DarkGray, ConsoleColor.DarkCyan,
             ConsoleColor.Cyan, ConsoleColor.Yellow, ConsoleColor.Red, ConsoleColor.Magenta, ConsoleColor.DarkBlue ];
 
-        public Action<int, string> LogFunction { get; }
-
         public WTelegramLogger(string accountName)
         {
-            _accountName = accountName = accountName ?? "undefined";
+            string path = Path.Combine(TelegramSessionsPaths.SessionsFolder, TelegramSessionsPaths.GetWTelegramLogFilePath(accountName));
+            _streamWriter = new StreamWriter(path, append: true);
+        }
 
-            _streamWriter = new StreamWriter(Path.Combine(TelegramSessionsPaths.SessionsFolder, TelegramSessionsPaths.GetWTelegramLogFilePath(accountName)), append: true);
-
-            LogFunction = (lvl, str) =>
+        static WTelegramLogger()
+        {
+            if (!Directory.Exists(TelegramSessionsPaths.SessionsFolder))
             {
-                lock (_logLock)
+                Directory.CreateDirectory(TelegramSessionsPaths.SessionsFolder);
+            }
+        }
+
+        public void LogFunction(int lvl, string str)
+        {
+            lock (_logLock)
+            {
+                if (lvl < 0)
                 {
-                    lock (_fileLock)
-                    {
-                        if (lvl < 0)
-                        {
-                            _streamWriter.WriteLine(str);
-                        }
-                        else
-                        {
-                            _streamWriter.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} | Log Level: {lvl} | {str}");
-                            _streamWriter.Flush();
-                        }
-                    }
-/*
-                    if (lvl > 2)
-                    {
-                        lock (_consoleLock)
-                        {
-                            ConsoleColor color = Console.ForegroundColor;
-                            Console.ForegroundColor = WTelegramConsoleColor[lvl];
-                            Console.WriteLine($"({_accountName}) {str}");
-                            Console.ForegroundColor = color;
-                        }
-                    }*/
+                    _streamWriter.WriteLine(str);
                 }
-            };
+                else
+                {
+                    _streamWriter.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} | Log Level: {lvl} | {str}");
+                    _streamWriter.Flush();
+                }
+
+                if (lvl > 2)
+                {
+
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = WTelegramConsoleColor[lvl];
+                    Console.WriteLine(str);
+                    Console.ForegroundColor = color;
+                }
+            }
         }
 
         public Action<int, string> GetLogFunction()
