30c9bab6cacf8d2c4cc779adc65a293610cbb893
Force <maxfunmaxfun672@gmail.com>
2024-10-20 00:51:04 +0300

    created Json models for payload creation reqs

---
+++ Blum/Models/Json/PayloadServerJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blum.Models.Json
+{
+    public class PayloadServerJson
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+    }
+
+    public class PayloadServersListJson
+    {
+        [JsonPropertyName("payloadServer")]
+        public List<PayloadServerJson> PayloadServers { get; set; } = [];
+    }
+}
---
+++ Blum/Models/Json/VercelAppResponseJson.cs
@@ -0,0 +1,13 @@
+﻿namespace Blum.Models.Json
+{
+    public class ErrorResponse
+    {
+        public VercelAppResponseJson? Error { get; set; } = null;
+    }
+
+    public class VercelAppResponseJson
+    {
+        public string? Code { get; set; } = null;
+        public string? Message { get; set; } = null;
+    }
+}
